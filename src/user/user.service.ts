157642403a98573956a9648193deb3a70bf788fa
Bashsoft707 <alausabashir@gmail.com>
2023-03-10 15:32:13 -0800

    removed used files, functions and variables

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,6 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { checkPassword, hashPassword } from 'src/utils/hash-password.utils';
 import { AccountService } from 'src/account/account.service';
 import { signToken } from 'src/utils/jwt.utils';
@@ -89,13 +88,5 @@ export class UserService {
     } catch (error) {
       throw new BadRequestException(error.message);
     }
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
   }
 }

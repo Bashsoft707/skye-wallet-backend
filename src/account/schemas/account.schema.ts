ac3864a74ebde1595f62c6e5e7b7a8faaf2b86e2
Bashsoft707 <alausabashir@gmail.com>
2023-03-11 19:09:22 +0100

    add swagger documentation

--- src/account/schemas/account.schema.ts
+++ src/account/schemas/account.schema.ts
@@ -1,20 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { User } from 'src/user/schemas/user.schema';
 
 export type AccountDocument = HydratedDocument<Account>;
 
 @Schema()
 export class Account {
+  @ApiProperty()
   @Prop({
     type: MongooseSchema.Types.ObjectId,
     ref: 'User',
   })
   profile: User;
 
+  @ApiProperty()
   @Prop()
   paymentID: string;
 
+  @ApiProperty()
   @Prop()
   balance: number;
 }
